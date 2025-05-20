ec3d70e904da66e818f70d572ab9971d1e155222
Naman Goyal <namangoyaldev@gmail.com>
2025-05-20 18:27:07 +0530

    feat: implement movie listing on home screen

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,28 +1,30 @@
-import { icons } from "@/constants/icons";
-import React from "react";
 import { Image, TextInput, View } from "react-native";
 
-type Props = {
-  onPress?: () => void;
-  placeholder: string;
-};
+import { icons } from "@/constants/icons";
 
-const SearchBar = ({ onPress, placeholder }: Props) => {
+interface Props {
+  placeholder: string;
+  value?: string;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, value, onChangeText, onPress }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
         source={icons.search}
-        className="size-5"
-        tintColor="#A8B5DB"
-        resize-mode="contain"
+        className="w-5 h-5"
+        resizeMode="contain"
+        tintColor="#AB8BFF"
       />
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
-        placeholderTextColor={"#A8B5DB"}
-        className="flex-1 text-white ml-5"
+        value={value}
+        onChangeText={onChangeText}
+        className="flex-1 ml-2 text-white"
+        placeholderTextColor="#A8B5DB"
       />
     </View>
   );
